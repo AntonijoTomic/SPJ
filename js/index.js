234c8ff44c189f879177284248dc3b1adb57c85c
AntonijoTomic <antonijo.tomic12@gmail.com>
2022-03-31 10:59:24 +0200

    treci commit

--- js/index.js
+++ js/index.js
@@ -10,6 +10,11 @@ var sRedak= '<tr>';
 sRedak += '<td>'+ element +'</td>';
 sRedak += '<td>'+element2+'</td>';
 sRedak += '<td>'+element3+'</td>';
+sRedak += '<td> <button type="button" onClick="deleteRow(this)" class="btn btn-danger">Obrisi</button></td>';
 $('#tablica-postovi tbody').prepend(sRedak);
 }
-
+function deleteRow(r)
+{
+var i=r.parentNode.parentNode.rowIndex;
+document.getElementById('tablica-postovi').deleteRow(i);
+}
